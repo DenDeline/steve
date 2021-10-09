d356935a217a54dc91d082ca666d5d7732e56c4f
Rostislav Statko <rostislav.statko@gmail.com>
2021-10-10 01:57:52 +0300

    feat: implement base bot features

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Bot } from 'grammy'
-
-const bot = new Bot(process.env.BOT_TOKEN || '')
-
-bot.command('start', ctx => ctx.reply("Hello world!"))
-
-bot.on('message', ctx => ctx.reply('Test dev'))
-
-bot.start()
